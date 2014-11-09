40ef0f7d118d0812f6d3ee92ca5b3f70e6db3fb4
bharathdintakurti <bharathdintakurti@outlook.com>
2014-11-09 21:29:43 +0530

    Sorting

--- Lists/stack.cpp
+++ Lists/stack.cpp
@@ -1,136 +1,254 @@
-#include<iostream>
-using namespace std;
-class node
-{
-      public:
-             node *next;
-             int data;
-};
+    #include<iostream>
+    #include<cstdio>
+    #include<cstdlib>
 
-int n;
+    using namespace std;
 
-class stack : public node
-{
-             node *head;
-             int tos,x;
-      	public:
-             stack()
-               {
-		            cout<<"\n\t\tOperations on Stacks\n";
-                    tos=-1;
-                    cout<<"\n\nEnter the size of the stack : ";
-                    cin>>n;
-               }
-             void push()
-              {
-              	if(tos < 0 )
-                  {
-                     cout <<"\n\nEnter a element to push: ";
-                     cin >> x;
-                     head =new node;
-                     head->next=NULL;
-                     head->data=x;
-                     tos ++;
-                   }
-             else
-                  {
-                   	 node *temp,*temp1;
-                     temp=head;
-                     if(tos >= (n-1))
-                     	{
-                      		cout <<"\n\nInvalid Operation.Stack Overflow\n";
-                        	return;
-                        }
-                        cout <<"\n\nEnter an element to push: ";
-                    	cin >> x;
-                     while(temp->next != NULL)
-                          temp=temp->next;
-                     temp1=new node;
-                     temp->next=temp1;
-                     temp1->next=NULL;
-                     temp1->data=x;
-                     tos++;
-                   }
-            }
-             void display()
-               {
-                  node *temp;
-                  temp=head;
-                  if (tos < 0)
-                    {
-                        cout <<"\nStack is empty\n";
-                        return;
-                    }
-                    cout<<"\n\nThe elements in stack are : ";
-                  while(temp != NULL)
-                     {
-                        cout <<temp->data<< " ";
-                        temp=temp->next;
-                     }
-                     cout<<"\n";
-               }
-              void pop()
-                {
-                   node *temp;
-                   temp=head;
-                   if( tos < 0 )
-                    {
-                       cout <<"\nInvalid Operation.Stack under-flow\n";
-                       return;
-                    }
-					if(tos==0)
-					{
-						cout<<"\n\nThe popped out element is :"<<temp->data;
-						temp->next=NULL;
-						tos--;
-						return ;
-					}
-				   if(tos>0)
-				   {
-				   		while(temp->next->next!=NULL)
-                   		{
-                    	 	temp=temp->next;
-                    	}
-                   		 if(temp->next->next==NULL)
-                    	{
-							cout<<"\n\nThe popped out element is :"<<temp->next->data;
-                   		}
-                    	temp->next=NULL;
-                    	tos--;
-                    	 return ;
-                   }
+class node{
+   public:
+        int info;
 
-                }
-};
+        struct node *next;
+
+    }*start;
+
+class stack{
+
+  private:
+        int size;
+  public:
+
+        node* create_node(int);
+
+        void create();
+
+        void push();
+
+       // void pop();
+
+        void search();
+
+        void update();
+
+        void display();
+
+        stack()
+
+        {
+
+            start = NULL;
+
+        }
+
+    };
 
 
-int main()
-{
-	stack s1;
-	int x;
-	int flag=0;
-	while(flag!=1)
-	{
-	    cout << "MENU\n";
-		cout <<"\n1.Push\n2.Pop\n3.Display\n4.Exit\n \tEnter your choice:";
-		cin>>x;
-		switch(x)
-	    {
-		case 1:
-                s1.push();
+  int main()
+
+    {
+
+        int choice, nodes, element, position, i;
+
+        stack sl;
+
+        start = NULL;
+
+        while (1)
+
+        {
+
+            cout<<endl<<"---------------------------------"<<endl;
+
+            cout<<endl<<"Operations on a Stack"<<endl;
+
+            cout<<endl<<"---------------------------------"<<endl;
+
+            cout<<"1.Create a stack"<<endl;
+
+            cout<<"2.Push an element on the stack"<<endl;
+
+            cout<<"3.Pop an element from the stack"<<endl;
+
+            cout<<"4.Search Element"<<endl;
+
+            cout<<"5.Display Stack"<<endl;
+
+            cout<<"6.Exit "<<endl;
+
+            cout<<"Enter your choice : ";
+
+            cin>>choice;
+
+            switch(choice)
+
+            {
+
+            case 1:
+
+                cout<<"Creating  Stack ....."<<endl;
+
+                sl.create();
+
+                cout<<endl;
+
                 break;
 
-		case 2:  s1.pop();
-			     break;
+            case 2:
+                cout<<"Pushing an element on the stack ....... "<<endl;
 
-		case 3:  s1.display();
-                	break;
+                sl.push();
 
-		case 4:  flag =1;
+                cout<<endl;
 
-		default:  cout<<"\nInvalid input.Try again\n";
-			break;
-		}
-     }
-return 0;
+                break;
+
+            case 3:
+                cout<<"Popping an element from the stack ........ "<<endl;
+
+              //  sl.pop();
+
+                cout<<endl;
+
+                break;
+            case 4:
+                cout<<"Displaying stack........ "<<endl;
+
+                sl.display();
+
+                cout<<endl;
+
+                break;
+
+
+
+            }
+        }
+
+
+ return 0;
+}
+
+
+void stack::create()
+{
+
+    cout << "Enter the size of the stack :";
+    cin >> size;
+
+    start = NULL;
+
+}
+
+
+
+
+
+
+node * stack ::create_node(int value)
+
+    {
+
+        struct node *temp, *s;
+
+        temp = new(struct node);
+
+        if (temp == NULL || size == 0)
+
+        {
+            start++ ;
+            cout<<"Stack Full.Memory not allocated "<<endl;
+
+
+
+        }
+
+        else
+
+        {
+
+            size--;
+
+            temp->info = value;
+
+            temp->next = NULL;
+
+            cout<<"Element pushed on to the stack"<<endl;
+
+            return temp;
+
+        }
+
+}
+
+
+void stack::push()
+
+    {
+
+        int value;
+
+        cout<<"Enter the value to be inserted: ";
+
+        cin>>value;
+
+        struct node *temp, *p;
+
+        temp = create_node(value);
+
+        if (start == NULL)
+
+        {
+
+            start = temp;
+
+            start->next = NULL;
+
+        }
+
+        else
+
+        {
+
+            p = start;
+
+            start = temp;
+
+            start->next = p;
+
+        }
+
+}
+
+void stack::display(){
+
+
+        struct node *temp;
+
+        if (start == NULL)
+
+        {
+
+            cout<<"The List is Empty"<<endl;
+
+            return;
+
+        }
+
+        temp = start;
+
+        cout<<"Elements of list are: "<<endl;
+
+        while (temp != NULL)
+
+        {
+
+            cout<<temp->info<<"->";
+
+            temp = temp->next;
+
+        }
+
+        cout<<"NULL"<<endl;
+
+
 }
